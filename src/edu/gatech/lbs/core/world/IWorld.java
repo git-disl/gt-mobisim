043e4159d04c1018d4e6e520e62426e73431b23c
Yanzhao Wu <yanzhaowu@gatech.edu>
2021-02-15 14:54:38 -0500

    delete old files

--- src/edu/gatech/lbs/core/world/IWorld.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2012, Georgia Tech Research Corporation
-// Authors:
-//   Peter Pesti (pesti@gatech.edu)
-//
-package edu.gatech.lbs.core.world;
-
-public interface IWorld {
-  public BoundingBox getBounds();
-}
